b80269cd1f8ec59854df84cc0956851a9fdeb7ca
thebhatman <manjunathbhat9920@gmail.com>
2019-08-19 16:35:03 +0530

    Added data_loader

---
+++ data_loader.jl
@@ -0,0 +1,30 @@
+using Base.Iterators: partition
+using Images
+
+function img_load(img_name)
+    img = load(img_name)
+    img = imresize(img, 64, 64)
+    img = reshape(Float64.(channelview(img)), 64, 64, 3)
+    return img
+end
+
+function load_dataset_as_batches(path, BATCH_SIZE)
+    data = []
+    i = 0
+    for r in readdir(path)
+        img_path = string(path, r)
+        push!(data, img_load(img_path))
+        i+=1
+        if i == 40960
+            break
+        end
+    end
+    num_images = length(data)
+    #println(num_images)
+    batched_data = []
+    for x in partition(data, BATCH_SIZE)
+        x = reshape(cat(x..., dims = 4), 64, 64, 3, BATCH_SIZE)
+        push!(batched_data, x)
+    end
+    return batched_data
+end
